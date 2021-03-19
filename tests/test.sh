3223271527c85a5bee488b8d3c7622f423e12136
Kimberly Robasky <krobasky@gmail.com>
2021-03-19 01:33:39 -0400

    docker tests fixed by delaying container exit with timeout flag

--- tests/test.sh
+++ tests/test.sh
@@ -5,6 +5,6 @@ set -e
 export $(cat tests/test.env|grep -v '^#')
 export TEST_LIBRARY=0
 
-docker-compose -f docker-compose.yml -f tests/docker-compose.yml up --build -V 
+docker-compose -f docker-compose.yml -f tests/docker-compose.yml up --build -V  --timeout 600 --exit-code-from fuse-server-immunespace-test
 
 #fuse-server-immunespace 
--- tests/test_func.py
+++ tests/test_func.py
@@ -36,12 +36,15 @@ json_headers = {
 
 # other endpoint tests, start with "test_"
 
+import numpy as np
+#g_debug = True
+g_debug = False
 def test_object():
 
     if os.getenv('TEST_LIBRARY') == "1":
         pytest.skip("Only testing docker lib")
 
-    objectId = os.getenv('GROUP')
+    objectId = "cellfie_group2"
     sess = os.getenv('APIKEY')
 
     log.info(f"Asking for ({objectId}) with API key ({sess})")
@@ -50,6 +53,29 @@ def test_object():
 
     with open('tests/expected/test_1.json', 'r', encoding='utf-8') as f:
         expected = json.load(f)
+
+
+    if(g_debug):
+        max_subjs=3
+        max_pheno=4
+        max_genes=5
+        obj["resource"]["exprs"] = np.array(obj["resource"]["exprs"])[0:max_genes,0:max_subjs].tolist() # 3 genes, 2 subjects
+        obj["resource"]["featureNames"] = np.array(obj["resource"]["featureNames"])[0:max_genes].tolist()
+        obj["resource"]["pData"] = np.array(obj["resource"]["pData"])[0:max_pheno,0:max_subjs].tolist() # 4 phenoetypes, 2 subjects
+        
+        expected["resource"]["exprs"] = np.array(expected["resource"]["exprs"])[0:max_genes,0:max_subjs].tolist() # 3 genes, 2 subjects
+        expected["resource"]["featureNames"] = np.array(expected["resource"]["featureNames"])[0:max_genes].tolist()
+        expected["resource"]["pData"] = np.array(expected["resource"]["pData"])[0:max_pheno,0:max_subjs].tolist() # 4 phenoetypes, 2 subjects
+        
+    objs = json.dumps(obj, ensure_ascii=False, indent=4, sort_keys=True)
+    expecteds = json.dumps(expected, ensure_ascii=False, indent=4, sort_keys=True)
+
+    if(g_debug):
+        print("obj:")
+        print(obj["resource"]["exprs"])
+        #print("expected:")
+        #print(expected["resource"]["exprs"])
+        
 
     objs = json.dumps(obj, ensure_ascii=False, indent=4, sort_keys=True)
     expecteds = json.dumps(expected, ensure_ascii=False, indent=4, sort_keys=True)
